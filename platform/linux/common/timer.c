d1fa49cf5d124c8d0279a5dd324ba953759af79f
Mike Mitchell <mike.mitchell@panometric.net>
2020-05-29 15:10:07 -0700

    First change proposed by Redpine. Pairs with Redpine 81df4a0b4515a92fb4cd9e86135681844d5e2e6b

--- platform/linux/common/timer.c
+++ platform/linux/common/timer.c
@@ -21,60 +21,47 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+#include "rsi_driver.h"
+#include "rsi_timer.h"
+#include "rsi_hal.h"
 #include <stddef.h>
-#include <sys/types.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <unistd.h>
 
 #include "timer_platform.h"
 
-bool has_timer_expired(Timer *timer) {
-	struct timeval now, res;
-	gettimeofday(&now, NULL);
-	timersub(&timer->end_time, &now, &res);
-	return res.tv_sec < 0 || (res.tv_sec == 0 && res.tv_usec <= 0);
+bool has_timer_expired(Timer *timer) 
+{
+  long left = timer->end_time - rsi_timer_read_counter();
+	return (left < 0);
 }
 
-void countdown_ms(Timer *timer, uint32_t timeout) {
-	struct timeval now;
-#ifdef __cplusplus
-	struct timeval interval = {timeout / 1000, static_cast<int>((timeout % 1000) * 1000)};
-#else
-	struct timeval interval = {timeout / 1000, (int)((timeout % 1000) * 1000)};
-#endif
-	gettimeofday(&now, NULL);
-	timeradd(&now, &interval, &timer->end_time);
+void countdown_ms(Timer *timer, uint32_t timeout)
+{
+	timer->timeout =timeout;
+  timer->end_time = rsi_timer_read_counter() + timeout;
 }
 
-uint32_t left_ms(Timer *timer) {
-	struct timeval now, res;
-	uint32_t result_ms = 0;
-	gettimeofday(&now, NULL);
-	timersub(&timer->end_time, &now, &res);
-	if(res.tv_sec >= 0) {
-		result_ms = (uint32_t) (res.tv_sec * 1000 + res.tv_usec / 1000);
-	}
-	return result_ms;
+uint32_t left_ms(Timer *timer)
+{
+  long left = timer->end_time -rsi_timer_read_counter();
+  return (left < 0) ? 0 : left;
 }
 
-void countdown_sec(Timer *timer, uint32_t timeout) {
-	struct timeval now;
-	struct timeval interval = {timeout, 0};
-	gettimeofday(&now, NULL);
-	timeradd(&now, &interval, &timer->end_time);
+void countdown_sec(Timer *timer, uint32_t timeout) 
+{
+  timer->end_time = rsi_timer_read_counter() + (timeout * 1000);  
 }
 
-void init_timer(Timer *timer) {
-	timer->end_time = (struct timeval) {0, 0};
+void init_timer(Timer *timer) 
+{
+	
+  timer->end_time = 0;  
 }
 
 void delay(unsigned milliseconds)
 {
-	useconds_t sleepTime = (useconds_t)(milliseconds * 1000);
-
-	usleep(sleepTime);
+  rsi_delay_ms(milliseconds);
 }
 
 #ifdef __cplusplus
